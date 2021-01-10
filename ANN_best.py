7e0abd92e1714e7dc1dda5d7c681dddc38f6926a
Triantafyllos Samoladas <triasamo1@ee.duth.gr>
2021-01-11 00:15:17 +0200

    Add Rest of Dataset Show Occurences for Dataset Imbalance Optimal ANN

--- ANN_best.py
+++ ANN_best.py
@@ -14,13 +14,10 @@ from tensorflow_addons.metrics import F1Score
 from keras.utils import np_utils
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
-
 from keras.preprocessing.image import ImageDataGenerator
-# TODO: add callbacks to stop training at certain achievement
-# TODO: L2 Regularization to stop overfitting
 
+# ---------  Preprocess Dataset ---------
 
-# training and testing path
 train_path = "fruits-360/Training"
 valid_path = "fruits-360/Test"
 
@@ -37,7 +34,15 @@ for fruit_dir_path in glob(train_path + "/*"):
         fruit_images.append(image)
         labels.append(fruit_label)
 fruit_images = np.array(fruit_images)
+labels_count = labels
 labels = np.array(labels)
+
+# ========================================
+print("Class Occurences in Dataset")
+for lab in np.unique(labels):
+    print("{0} : {1} ".format(lab,labels_count.count(lab)))
+
+# ========================================
 
 label_to_id_dict = {v:i for i,v in enumerate(np.unique(labels))}
 id_to_label_dict = {v: k for k, v in label_to_id_dict.items()}
@@ -65,20 +70,20 @@ validation_label_ids = np.array([label_to_id_dict[x] for x in validation_labels]
 X_train, X_test = fruit_images, validation_fruit_images
 Y_train, Y_test = label_ids, validation_label_ids
 
-# Normalize color values to between -1 and 1
+# Normalize color values to  [-1,1]
 X_train = np.subtract(X_train/127.5, 1)
 X_test = np.subtract(X_test/127.5, 1)
 
-# Make a flattened version for some of our models
+# Flatten input
 X_train = X_train.reshape(X_train.shape[0], 50*50*3)
 X_test = X_test.reshape(X_test.shape[0], 50*50*3)
 
-# One Hot Encode the Output
+# OneHot Encode the Output
 Y_train = np_utils.to_categorical(Y_train, 8)
 Y_test = np_utils.to_categorical(Y_test, 8)
 print(X_train.shape)
 
-# construct model
+# ---------  Construct Model ---------
 model = Sequential([
     Dense(64, input_shape=(7500,),kernel_regularizer=L2(0.001)),            # <-- Make our network smaller/simpler
     BatchNormalization(),
@@ -105,6 +110,8 @@ history = model.fit(X_train, Y_train, batch_size=64, epochs=4)
 print('Training Finished..')
 print('Testing ..')
 
+# --------- Test set  ---------
+
 score = model.evaluate(X_test, Y_test)
 
 print('===Testing Metrics===')
@@ -113,6 +120,8 @@ print('Test accuracy: ', score[1])
 print('Test precision: ', score[2])
 print('Test recall: ', score[3])
 print('Test F1 Score: ', score[4])
+
+# ---------  Confusion Matrix ---------
 
 y_pred = model.predict(X_test)
 y_pred = np.argmax(y_pred, axis=-1)
@@ -131,6 +140,7 @@ plt.ylabel('True label')
 plt.xlabel('Predicted label')
 plt.show()
 
+# ---------  Accuracy - Loss Plot ---------
 fit_hist = pd.DataFrame(history.history)
 
 loss = round(np.min(fit_hist['loss']), 2)
